b284f69726869ea7e4c00fb35bc5fb7254de76ae
Pedro Henrique Mittmann Hahn <pedro.298521@alunosatc.edu.br>
2024-11-27 20:03:00 -0300

    kanye-api

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,39 +1,48 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import http from 'k6/http';
-import { check, sleep } from 'k6';
-import { Trend } from 'k6/metrics';
+import { check } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
+// Métricas
+export const getContactsDuration = new Trend('get_contacts_duration', true);
+export const getContactsFailedRate = new Rate('get_contacts_failed_rate');
 
+// Configurações de carga
 export const options = {
+  stages: [
+    { duration: '1m', target: 20 }, // Gradual aumento inicial
+    { duration: '1m', target: 100 },
+    { duration: '1m', target: 150 },
+    { duration: '1m', target: 250 },
+    { duration: '1m', target: 300 } // Pico de 300 VUs
+  ],
   thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['avg<10000']
+    http_req_failed: ['rate<0.12'], // Menos de 12% de erros
+    http_req_duration: ['p(95)<5700'] // 95% das requisições abaixo de 5700ms
   }
 };
 
+// Geração de relatório
 export function handleSummary(data) {
   return {
     './src/output/index.html': htmlReport(data)
   };
 }
 
+// Teste principal
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://api.kanye.rest';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const res = http.get(baseUrl);
 
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
+  // Registrar duração da requisição
   getContactsDuration.add(res.timings.duration);
 
+  // Registrar taxa de falha com base no status
+  getContactsFailedRate.add(res.status !== 200);
+
+  // Verificar se a resposta está correta
   check(res, {
-    'get contacts - status 200': () => res.status === OK
+    'get contacts - status 200': r => r.status === 200
   });
 }
